749eb9852b433107cd06cd4e2e27b3cd9f5ef28a
fredericvauchelles <fredericvauchelles@.(none)>
2014-09-17 13:41:29 +0200

    WIP: package test

--- src/main/runtime_resources/Scripts/Editor/BuildPipelineTools.cs
+++ src/main/runtime_resources/Scripts/Editor/BuildPipelineTools.cs
@@ -1,14 +1,77 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MW
 {
 	public static class BuildPipelineTools
 	{
-		public static void ExportPackage()
+		public static void ExportPackageCL()
 		{
-			Debug.Log(string.Join(", ", System.Environment.GetCommandLineArgs()));
-			EditorApplication.Exit(-1);
+			Dictionary<string, List<string>> parsed = Parse(
+				System.Environment.GetCommandLineArgs(),
+				"+sourceAsset", "+output"
+				);
+
+			if (!parsed.ContainsKey("+sourceAsset") || parsed["+sourceAsset"].Count == 0) 
+			{
+				throw new UnityException("+sourceAsset requires at least one path");
+			}
+			string[] sourceAssets = parsed["+sourceAsset"].ToArray();
+
+			if (!parsed.ContainsKey("+output") || parsed["+output"].Count != 1) 
+			{
+				throw new UnityException("+output path must have exactly one path");
+			}
+			string output = parsed["+output"][0];
+
+			ExportPackage(sourceAssets, output, ExportPackageOptions.Recurse);
+		}
+
+		private static void ExportPackage(string[] sourceAssets, string output, ExportPackageOptions flags)
+		{
+			flags |= ExportPackageOptions.Recurse;
+			if (sourceAssets.Any(s => s.Contains("ProjectSettings")))
+			{
+				sourceAssets = sourceAssets.Where(s => !s.Contains("ProjectSettings")).ToArray();
+				flags |= ExportPackageOptions.IncludeLibraryAssets;
+			}
+			
+			Debug.Log("Generating package at: " + output);
+			Debug.Log("Options: " + flags);
+			foreach (string path in sourceAssets)
+			{
+				Debug.Log("Source Asset: " + path);
+			}
+			AssetDatabase.ExportPackage(sourceAssets, output, flags);
+		}
+
+		private static Dictionary<string, List<string>> Parse(IEnumerable<string> tokens, params string[] fields)
+		{
+			Dictionary<string, List<string>> result = new Dictionary<string, List<string>>();
+			IEnumerator<string> e = tokens.GetEnumerator();
+			bool doLoop = e.MoveNext();
+			while(doLoop)
+			{
+				if (fields.Contains(e.Current))
+				{
+					string key = e.Current;
+					if (!result.ContainsKey(key))
+					{
+						result.Add(key, new List<string>());
+					}
+					while ((doLoop = e.MoveNext()) && !fields.Contains(e.Current))
+					{
+						result[key].Add(e.Current);
+					}
+				}
+				else
+				{
+					doLoop = e.MoveNext();
+				}
+	       	}
+			return result;
 		}
 	}
 }
